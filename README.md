cfac028a87b033d2a4ca6e6cc5d05d2a4412a756
Aiven <47558328@QQ.COM>
2014-10-15 15:18:35 +0800

    created project

---
+++ README.md
@@ -0,0 +1,47 @@
+# Eyas UI #
+
+Eyas UI theme is a free UI component.  Official website: [http://www.56hm.com/Eyas-UI/](http://www.56hm.com/Eyas-UI/)
+
+
+# version #
+
+##### build: 1.0.0 #####
+
+
+
+
+# Environmental requirements #
+
+- Jquery 1.11.x
+
+
+To determine your PHP version, the PHP code to create a new file: '<? PHP echo php_version; / PHP 'version. This will print to the version number of your screen.
+
+# Installation #
+
+1. Make sure your development environment to meet the minimum configuration of the system required.
+
+2. [click here](https://github.com/yy47558328/Eyas-UI) or clone the GitHub repo.
+
+
+
+# problem? #
+
+If you have what problem can contact me。 
+  
+  QQ:47558328。
+  
+  Email: 47558328@qq.com, yy47558328@sina.com。
+  
+  blog : [http://weibo.com/56hm](http://weibo.com/56hm)。
+
+  website :  [http://www.56hm.com](http://www.56hm.com)。
+
+
+
+
+
+# copyright #
+Copyright (c) 2014-2018, 56HM Limited - [http://www.56hm.com](www.56hm.com)
+All rights reserved.
+
---
+++ components/EyasUI.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * EyasUI core component
+ */
+class EyasUI extends CApplicationComponent
+{
+
+	public $forceCopyAssets = false;
+
+	protected $_assetsUrl;
+
+	/**
+	 * 注册EyasUI所需文件.
+	 * @return [type] [description]
+	 */
+	public function register()
+	{
+        $this->registerCss();
+	}
+
+
+    /**
+     * 注册EyasUI所需CSS库.
+     * @return [type] [description]
+     */
+	public function registerCss()
+	{
+	   $filename = YII_DEBUG ? 'eyas-ui.css' : 'eyas-ui.min.css';
+       $css = Yii::app()->getClientScript();
+       Yii::app()->clientScript->registerCssFile($this->getAssetsUrl().'/css/'.$filename);
+	}
+
+    /**
+     * 注册EyasUI所需Javascript库.
+     * @return [type] [description]
+     */
+	public function registerScript()
+	{
+
+	}
+
+
+	protected function getAssetsUrl()
+	{
+		if (isset($this->_assetsUrl))
+			return $this->_assetsUrl;
+		else
+		{
+			$assetsPath = Yii::getPathOfAlias('eyasui.assets');
+			$assetsUrl = Yii::app()->assetManager->publish($assetsPath, true, -1, $this->forceCopyAssets);
+			return $this->_assetsUrl = $assetsUrl;
+		}
+	}
+}
